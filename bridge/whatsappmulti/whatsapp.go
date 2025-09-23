e34c1853a17f8323b75fd4e55db6145a0122c2a0
Tehseen <tehsu@snyde.net>
2025-09-23 12:32:44 -0500

    Add context to GetJoinedGroups call

--- bridge/whatsappmulti/whatsapp.go
+++ bridge/whatsappmulti/whatsapp.go
@@ -128,7 +128,7 @@ func (b *Bwhatsapp) Connect() error {
 		return errors.New("failed to get contacts: " + err.Error())
 	}
 
-	b.joinedGroups, err = b.wc.GetJoinedGroups()
+	b.joinedGroups, err = b.wc.GetJoinedGroups(context.Background())
 	if err != nil {
 		return errors.New("failed to get list of joined groups: " + err.Error())
 	}
